f3acacbdba81a10befe53f78de481960d5323460
Erin-Rooney <61806923+Erin-Rooney@users.noreply.github.com>
2021-05-27 21:19:54 -0700

    Update d-functions_stats.R

--- code/drake_functions/d-functions_stats.R
+++ code/drake_functions/d-functions_stats.R
@@ -26,3 +26,31 @@
 # 1. PROCESSING FUNCTIONS -------------------------------------------------
 ## LEVEL I FUNCTIONS -------------------------------------------------------
 
+permanova = function(p_relabund_by_sample){
+relabund_wide_sample =
+  p_relabund_by_sample %>%
+  ungroup %>% 
+  select(X, time, ctrt, ftrt, phosphorus_pool, relabund) %>% 
+  mutate(phosphorus_pool = recode(phosphorus_pool, "pbic_percbio" = "Available P",
+                                  'amac_percbio' = "Reserve P",
+                                  'unavp_percbio' = "Fixed P",
+                                  'porg_percbio' = "Organic P")) %>% 
+  mutate(phosphorus_pool = factor(phosphorus_pool, levels = c('Available P', "Reserve P", "Organic P", "Fixed P"))) %>%
+  pivot_wider(names_from = "phosphorus_pool", values_from = "relabund") %>%
+  filter(ctrt != "Control" & time != "Baseline") %>% 
+  replace(is.na(.),0) 
+
+
+library(vegan)
+permanova_ftrt_ctrt_all = 
+  adonis(relabund_wide_sample %>% select(c(`Available P`, `Reserve P`, `Organic P`, `Fixed P`)) ~ 
+           (ctrt*ftrt*time), 
+         data = relabund_wide_sample) 
+
+
+
+permatable = permanova_ftrt_ctrt_all$aov.tab
+
+write.csv(permatable, "permatable.csv", row.names = TRUE)
+
+}
